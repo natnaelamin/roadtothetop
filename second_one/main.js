97c521d150d6f03828642c880409e3a10808004f
Natnael Amin <99741416+natnaelamin@users.noreply.github.com>
2023-03-03 02:12:06 +0300

    Add files via upload

---
+++ second_one/main.js
@@ -0,0 +1,125 @@
+// customer Class
+class Customer {
+    constructor(name, location, phone) {
+      this.name = name;
+      this.location = location;
+      this.phone = phone;
+    }
+  }
+  // UI Class
+  class UI {
+    static displaycustomers() {
+      const customers = Store.getcustomers();
+      customers.forEach((customer) => UI.addCustomerToList(customer));
+    }
+    static addCustomerToList(customer) {
+      const list = document.querySelector("#customer-list");
+      const row = document.createElement("tr");
+      row.innerHTML = `
+      <td>${customer.name}</td>
+      <td>${customer.location}</td>
+      <td>${customer.phone}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+      list.appendChild(row);
+    }
+    static deleteCustomer(el) {
+      if (el.classList.contains("delete")) {
+        el.parentElement.parentElement.remove();
+      }
+    }
+    static showAlert(message, className) {
+      const div = document.createElement("div");
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector(".container");
+      const form = document.querySelector("#customer-form");
+      container.insertBefore(div, form);
+      // Vanish in 2 seconds
+      setTimeout(() => document.querySelector(".alert").remove(), 2000);
+    }
+  
+    static clearFields() {
+      document.querySelector("#name").value = "";
+      document.querySelector("#location").value = "";
+      document.querySelector("#phone").value = "";
+    }
+  }
+  // Store Class
+  class Store {
+    static getcustomers() {
+      let customers;
+      if (localStorage.getItem("customers") === null) {
+        customers = [];
+      } else {
+        customers = JSON.parse(localStorage.getItem("customers"));
+      }
+      return customers;
+    }
+    static addCustomer(customer) {
+      const customers = Store.getcustomers();
+      customers.push(customer);
+      localStorage.setItem("customers", JSON.stringify(customers));
+    }
+    static removeCustomer(phone) {
+      const customers = Store.getcustomers();
+      customers.forEach((customer, index) => {
+        if (customer.phone === phone) {
+          customers.splice(index, 1);
+        }
+      });
+      localStorage.setItem("customers", JSON.stringify(customers));
+    }
+  }
+  // Event: Display customers
+  document.addEventListener("DOMContentLoaded", UI.displaycustomers);
+  // Event: Add a customer
+  document.querySelector("#customer-form").addEventListener("submit", (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+    // Get form values
+    const name = document.querySelector("#name").value;
+    const location = document.querySelector("#location").value;
+    const phone = document.querySelector("#phone").value;
+    // Validate
+    if (name === "" || location === "" || phone === "") {
+      UI.showAlert("you must fill in all fields", "danger");
+    }
+    else if (name.length < 4 || name.length > 15) {
+        UI.showAlert("name must be 4 to 15 characters long", "danger");}        
+    else if (isNaN(phone)=== true || phone.length !== 10){
+      UI.showAlert("phone# must be a number and 10 characters long", "danger");
+    }    
+    else if(location.length < 4 || location.length > 15){
+      UI.showAlert("location must be 4 to 15 characters long", "danger");
+    }
+    else {
+        
+      // Instantiate customer
+      const customer = new Customer(name, location, phone);
+      // Add customer to UI
+      UI.addCustomerToList(customer);
+      // Add customer to store
+      Store.addCustomer(customer);
+      // Show success message
+      UI.showAlert("customer Added ", "success");
+  
+      // Clear fieldsUI.clearFields();
+      UI.clearFields();
+    }
+  });
+  
+  // Event: Remove a customer
+  document.querySelector("#customer-list").addEventListener("click", (e) => {
+  
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure you wanna remove this person?')){
+    // Remove customer from UI
+    UI.deleteCustomer(e.target);
+    // Remove customer from store
+    Store.removeCustomer(
+      e.target.parentElement.previousElementSibling.textContent
+    );
+    // Show success message
+    UI.showAlert("customer Removed", "success");}}
+  });
