9840ad97d7733d444888946a7022f217e5297eb7
Natnael Amin <99741416+natnaelamin@users.noreply.github.com>
2023-04-04 01:37:04 +0300

    Update main.js

--- currency converter/main.js
+++ currency converter/main.js
@@ -1,10 +1,54 @@
-document.getElementById("output").style.visibility = "hidden";
-document.getElementById("dlinput").addEventListener("input", function (e) {
-  let dl = e.target.value;
-  document.getElementById("output").style.visibility = "visible";
+let api = `https://v6.exchangerate-api.com/v6/9a750063c69dd763342bc8af/latest/USD`;
+const fromDropDown = document.getElementById("from-select");
+const toDropDown = document.getElementById("to-select");
 
-  document.getElementById("pdoutput").innerHTML = dl * 0.83;
-
-  document.getElementById("euoutput").innerHTML = dl * 0.94;
-  document.getElementById("broutput").innerHTML = dl * 53.8;
+//Create dropdown from the currencies array
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
 });
+
+//Repeat same thing for the other dropdown
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
+
+//Setting default values
+fromDropDown.value = "USD";
+toDropDown.value = "ETB";
+
+let convertCurrency = () => {
+  //Create References
+  const amount = document.querySelector("#amount").value;
+  const fromCurrency = fromDropDown.value;
+  const toCurrency = toDropDown.value;
+
+
+  function fetchapi(){
+    fetch(api)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+          2
+        )} ${toCurrency}`;
+      });
+  }
+  //If amount input field is not empty
+  if (amount.length != 0) {
+    fetchapi();
+  } else {
+    alert("you have to fill in the amount");
+  }
+};
+
+document
+  .querySelector("#convert-button")
+  .addEventListener("click", convertCurrency);
